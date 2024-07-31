e7571dac02554ae353d56e3679383a234045a074
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2024-07-31 19:38:04 +0000

    StreamlitDashboard: added custom data price prediction section

--- app_pages/app_predictive_analysis.py
+++ app_pages/app_predictive_analysis.py
@@ -17,7 +17,6 @@ def app_predictive_analysis_page():
 
     if st.checkbox("Inspect Inherited Housing Records"):
         data = load_inherited_dataset()
-        data = data.drop(columns=['EnclosedPorch', 'GarageFinish', 'LotFrontage', 'WoodDeckSF'])
         st.write(f"* The dataset has {data.shape[0]} rows, across {data.shape[1]} features.\n\n")
         st.write(data)
 
@@ -35,15 +34,19 @@ def app_predictive_analysis_page():
 
     live_data = create_input_widgets()
 
+    if st.button("Predict Sale Price"): 
+        st.write(live_data)
+        predict_sale_price(live_data, pipeline)
+
 def create_input_widgets():
 
     original_housing_data = load_house_dataset()
     percentageMin, percentageMax = 0.3, 1.5
 
-    feature_1, feature_2, feature_3, feature_4, feature_5 = st.beta_columns(5)
-    feature_6, feature_7, feature_8, feature_9, feature_10 = st.beta_columns(5)
-    feature_11, feature_12, feature_13, feature_14, feature_15 = st.beta_columns(5)
-    feature_16, feature_17, feature_18, feature_19 = st.beta_columns(4)
+    feature_1, feature_2, feature_3, feature_4, feature_5, feature_6 = st.beta_columns(6)
+    feature_7, feature_8, feature_9, feature_10, feature_11, feature_12 = st.beta_columns(6)
+    feature_13, feature_14, feature_15, feature_16, feature_17, feature_18 = st.beta_columns(6)
+    feature_19, feature_20, feature_21, feature_22, feature_23 = st.beta_columns(5)
     
 
     live_data = pd.DataFrame([], index=[0])
@@ -88,7 +91,7 @@ def create_input_widgets():
 	    feature = "BsmtExposure"
 	    st_widget = st.selectbox(
 			label= feature,
-			options= ['gd', 'av', 'mn', 'no', 'none']     
+			options= ['Gd', 'Av', 'Mn', 'No', 'None']     
 			)
             
     live_data[feature] = st_widget
@@ -139,6 +142,15 @@ def create_input_widgets():
     live_data[feature] = st_widget
 
     with feature_9:
+	    feature = "GarageFinish"
+	    st_widget = st.selectbox(
+			label= feature,
+			options= ['Fin', 'RFn', 'Unf', 'None']     
+			)
+
+    live_data[feature] = st_widget
+
+    with feature_10:
 	    feature = "GarageYrBlt"
 	    st_widget = st.number_input(
 			label= feature,
@@ -150,7 +162,7 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_10:
+    with feature_11:
 	    feature = "GrLivArea"
 	    st_widget = st.number_input(
 			label= feature,
@@ -162,7 +174,7 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_11:
+    with feature_12:
 	    feature = "KitchenQual"
 	    st_widget = st.selectbox(
 			label= feature,
@@ -171,7 +183,7 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_12:
+    with feature_13:
 	    feature = "LotArea"
 	    st_widget = st.number_input(
 			label= feature,
@@ -183,7 +195,19 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_13:
+    with feature_14:
+	    feature = "LotFrontage"
+	    st_widget = st.number_input(
+			label= feature,
+			min_value= int(original_housing_data[feature].min()*percentageMin), 
+			max_value= int(original_housing_data[feature].max()*percentageMax),
+			value= int(original_housing_data[feature].median()), 
+            step = 10       
+			)
+            
+    live_data[feature] = st_widget
+
+    with feature_15:
 	    feature = "MasVnrArea"
 	    st_widget = st.number_input(
 			label= feature,
@@ -195,7 +219,19 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_14:
+    with feature_16:
+	    feature = "EnclosedPorch"
+	    st_widget = st.number_input(
+			label= feature,
+			min_value= int(original_housing_data[feature].min()*percentageMin), 
+			max_value= int(original_housing_data[feature].max()*percentageMax),
+			value= int(original_housing_data[feature].median()), 
+            step = 10      
+			)
+            
+    live_data[feature] = st_widget
+
+    with feature_17:
 	    feature = "OpenPorchSF"
 	    st_widget = st.number_input(
 			label= feature,
@@ -207,7 +243,7 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_15:
+    with feature_18:
 	    feature = "OverallCond"
 	    st_widget = st.number_input(
 			label= feature,
@@ -217,9 +253,9 @@ def create_input_widgets():
             step = 1       
 			)
 
-    live_data[feature] = st_widget
+    live_data[feature] = st_widget    
     
-    with feature_16:
+    with feature_19:
 	    feature = "OverallQual"
 	    st_widget = st.number_input(
 			label= feature,
@@ -231,7 +267,7 @@ def create_input_widgets():
 
     live_data[feature] = st_widget
 
-    with feature_17:
+    with feature_20:
 	    feature = "TotalBsmtSF"
 	    st_widget = st.number_input(
 			label= feature,
@@ -243,7 +279,19 @@ def create_input_widgets():
 
     live_data[feature] = st_widget
 
-    with feature_18:
+    with feature_21:
+	    feature = "WoodDeckSF"
+	    st_widget = st.number_input(
+			label= feature,
+			min_value= int(original_housing_data[feature].min()*percentageMin), 
+			max_value= int(original_housing_data[feature].max()*percentageMax),
+			value= int(original_housing_data[feature].median()), 
+            step = 25       
+			)
+
+    live_data[feature] = st_widget
+
+    with feature_22:
 	    feature = "YearBuilt"
 	    st_widget = st.number_input(
 			label= feature,
@@ -255,7 +303,7 @@ def create_input_widgets():
             
     live_data[feature] = st_widget
 
-    with feature_19:
+    with feature_23:
 	    feature = "YearRemodAdd"
 	    st_widget = st.number_input(
 			label= feature,
